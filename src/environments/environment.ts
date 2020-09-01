562ae67677bff11415cc459f86eb021b7757c131
RicardoPineda2301 <rp230198@gmail.com>
2020-09-01 14:12:02 -0600

    Agregando faltantes

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCxl6uSZioMvqWG-0y1kSffG3bDvLieEFw",
+    authDomain: "fir-auth-50b1d.firebaseapp.com",
+    databaseURL: "https://fir-auth-50b1d.firebaseio.com",
+    projectId: "fir-auth-50b1d",
+    storageBucket: "fir-auth-50b1d.appspot.com",
+    messagingSenderId: "1095273939746",
+    appId: "1:1095273939746:web:4403aeb9e54696d814db7b"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
