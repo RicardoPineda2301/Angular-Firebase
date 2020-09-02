3c9f7e5ac163ad08d5194ec620b4a25ed45fade5
Ricardo Pineda <41968210+RicardoPineda2301@users.noreply.github.com>
2020-09-02 12:10:02 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCxl6uSZioMvqWG-0y1kSffG3bDvLieEFw",
-    authDomain: "fir-auth-50b1d.firebaseapp.com",
-    databaseURL: "https://fir-auth-50b1d.firebaseio.com",
-    projectId: "fir-auth-50b1d",
-    storageBucket: "fir-auth-50b1d.appspot.com",
-    messagingSenderId: "1095273939746",
-    appId: "1:1095273939746:web:4403aeb9e54696d814db7b"
+   // Su llave API de Firebase va aqui   
   }
 };
 
