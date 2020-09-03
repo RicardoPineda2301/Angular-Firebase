1f61dce7cc80ef58ed12b8aaeb943b8b34187bd6
RicardoPineda2301 <rp230198@gmail.com>
2020-09-03 11:52:18 -0600

    Manejo de errores inicial y paquetes de manejo de autenticacion

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email = "";
+  password = "";
+  errorMessage = ""; //Validacion para el manejo de errores
+  error: {name:string, message:string} = { name: "", message: ""} //Manejo de errores en Firebase
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  register(){
+    alert("Ahuevos que si papaaaa")
   }
 
 }
