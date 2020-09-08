5c5ea8f5e5d0acaf0bbda558d0d69c585915e468
RicardoPineda2301 <rp230198@gmail.com>
2020-09-08 07:51:18 -0600

    gets y funciones

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +11,51 @@ export class LoginComponent implements OnInit {
 
 email = "";
 password = "";
+errorMessage = '';
+error : { name:string, message:string } = {name: '', message: ''}
 
-  constructor() { }
+  constructor(private authservice: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  
+  clearError(){
+    this.errorMessage = '';
+    this.error = {name: '', message: ''};
+  }
 
   login(){
-    alert("ahuevos")
+    alert("funciona")
+  }
+
+  register(){
+    this.clearError()
+    if(this.validateForm(this.email, this.password)){
+      this.authservice.loginWithEmail(this.email, this.password).then(()=>{
+        this.router.navigate(['/userinfo'])
+      }).catch(_error =>{
+        this.error = _error,
+        this.router.navigate(['/login'])
+      })
+    }
+  }
+
+  validateForm(email, password){
+    if(email.length === 0){
+      this.errorMessage = 'Ponga un correo valido';
+      return false;
+    }
+    if(password.length === 0){
+      this.errorMessage = 'Ponga una contraseña valida';
+      return false;
+    }
+
+    if(password.length < 6){
+      this.errorMessage = 'La contraseña elegida es muy debil. Por favor ponga una que tenga al menos 6 caracteres';
+      return false;
+    }
+
+    this.errorMessage = '';
+    return true;
   }
 }
